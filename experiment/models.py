ff00384db5e48bf4c2d2295efb036bd426788b0b
Nickolai Tchesnokov <nickolaitchesnokov@dyn896-244.eduroam.ic.ac.uk>
2023-02-04 23:54:04 +0000

    experiment model, serializers & get_experiments view

--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Experiment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    date_created = models.DateField(auto_now_add=True)
+    num_of_groups = models.IntegerField()
+    num_of_samples = models.IntegerField()
+    
--- experiment/views.py
+++ experiment/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Experiment
+from .serializers import ExperimentSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_experiments(request):
+    experiments = Experiment.objects.all()
+    serializer = ExperimentSerializer(experiments, many=True)
+    return Response(serializer.data)
