c6380fc28dcbf7fec590d5159e6197cffc03d44e
Nickolai Tchesnokov <nickolaitchesnokov@dyn896-244.eduroam.ic.ac.uk>
2023-02-05 01:11:10 +0000

    experiment get & post views

--- experiment/models.py
+++ experiment/models.py
@@ -2,12 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Experiment(models.Model):
+
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
     date_created = models.DateField(auto_now_add=True)
-    num_of_groups = models.IntegerField()
-    num_of_participants = models.IntegerField()
-
 
 class ExperimentGroup(models.Model):
     name = models.CharField(max_length=100)
@@ -17,6 +15,7 @@ class Participant(models.Model):
     name = models.CharField(max_length=100)
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
     experiment_group = models.ForeignKey(ExperimentGroup, on_delete=models.CASCADE)
+    user_id = models.CharField(max_length=300)
 
 
 
--- experiment/views.py
+++ experiment/views.py
@@ -1,29 +1,107 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Experiment
-from .serializers import ExperimentSerializer
+from .models import Experiment, ExperimentGroup, Participant
+from .serializers import ExperimentSerializer, ExperimentGroupSerializer, ParticipantSerializer
 
 @api_view(['GET'])
 def get_experiments(request):
     experiments = Experiment.objects.all()
-    serializer = ExperimentSerializer(experiments, many=True)
-    return Response(serializer.data)
+
+    response = []
+    for experiment in experiments:
+        experiment = get_exp(experiment)
+        response.append(experiment)
+
+    return Response(response)
 
 @api_view(['GET'])
 def get_experiment(request, experiment_id):
     experiment = Experiment.objects.get(id=experiment_id)
-    serializer = ExperimentSerializer(experiment, many=False)
-    return Response(serializer.data)
+    experiment_serializer = ExperimentSerializer(experiment, many=False)
+
+    groups = ExperimentGroup.objects.filter(experiment=experiment)
+    groups_serializer = ExperimentGroupSerializer(groups, many=True)
+
+    particpants = Participant.objects.filter(experiment=experiment)
+    particpants_serializer = ParticipantSerializer(particpants, many=True)
+    
+    experiment = {
+        **experiment_serializer,
+        'groups': groups_serializer,
+        'participants': particpants_serializer,
+    }
+
+    return Response(experiment)
 
 @api_view(['POST'])
 def add_experiment(request):
-    experiment = Experiment(
-        name=request.data['name'], 
-        description=request.data['description'], 
-        num_of_groups=request.data['num_of_groups'], 
-        num_of_participants=request.data['num_of_participants'])
-    experiment.save()
-    return Response({'status':True})
 
+    try:
+        experiment = Experiment(
+            name=request.data['name'], 
+            description=request.data['description'],)
+        experiment.save()
+
+        response = {
+            'status': True,
+            'experiment_id': experiment.id,
+        }
+        return Response(response)
+
+    except:
+        return Response({'status':False})
+
+@api_view(['POST'])
+def add_experiment_group(request):
+    
+    try:
+        experiment_group = ExperimentGroup(
+            name=request.data['name'],
+            experiment=request.data['experiment'],)
+        experiment_group.save()
+
+        response = {
+            'status': True,
+            'experiment_group_id': experiment_group.id,
+        }
+        return Response(response)
+
+    except:
+        return Response({'status':False})
+
+@api_view(['POST'])
+def add_participant(request):
+
+    try:
+        participant = Participant(
+            name=request.data['name'],
+            experiment=request.data['experiment'],
+            experiment_group=request.data['experiment_group'],
+            user_id=request.data['user_id'],)
+        participant.save()
+
+        response = {
+            'status': True,
+            'participant_id': participant.id,
+        }
+        return Response(response)
+    except:
+        return Response({'status':False})
+
+
+def get_exp(experiment):
+
+    experiment_serializer = ExperimentSerializer(experiment, many=False)
+
+    groups = ExperimentGroup.objects.count(experiment=experiment)
+    participants = Participant.objects.count(experiment=experiment)
+
+    experiment = {
+        **experiment_serializer,
+        'num_groups': groups,
+        'num_participants': participants,
+    } 
+
+    return experiment
 
