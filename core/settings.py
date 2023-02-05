937803cb17458eb7237bbf24a54e03960bda6c44
calebgcc <calebgucciardi@gmail.com>
2023-02-05 01:21:43 +0000

    migrations

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": "mediserv",
+        "USER": "calebgucciardi",
+        "PASSWORD": "$$root$$",
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
 
