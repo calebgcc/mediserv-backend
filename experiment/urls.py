1897470dd7472f05ed4ee23c9dc84a63831b2755
Nickolai Tchesnokov <nickolaitchesnokov@dyn896-244.eduroam.ic.ac.uk>
2023-02-05 01:33:19 +0000

    updated urls

--- experiment/urls.py
+++ experiment/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('experiments/', views.get_experiment, name='experiments'),
+    path('experiments/', views.get_experiments, name='experiments'),
     path('experiments/<experiment_id>/', views.get_experiment, name='experiment'),
     path('experiments/add_experiment/', views.add_experiment, name='add-experiment'),
 ]
