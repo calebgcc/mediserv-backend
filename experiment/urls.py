c84603c5cf34f47028d3c75033126d022013d97b
calebgcc <calebgucciardi@gmail.com>
2023-02-05 01:36:01 +0000

    solved add_experiment problem

--- experiment/urls.py
+++ experiment/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('experiments/', views.get_experiments, name='experiments'),
-    path('experiments/<experiment_id>/', views.get_experiment, name='experiment'),
     path('experiments/add_experiment/', views.add_experiment, name='add-experiment'),
+    path('experiments/<experiment_id>/', views.get_experiment, name='experiment'),
 ]
 
 
