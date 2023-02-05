8a54c611ce8e05a673674ab725b5b6842f186b20
Nickolai Tchesnokov <nickolaitchesnokov@dyn896-244.eduroam.ic.ac.uk>
2023-02-05 04:04:22 +0000

    added parameters to Experiment model

--- experiment/models.py
+++ experiment/models.py
@@ -5,6 +5,18 @@ class Experiment(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
     date_created = models.DateField(auto_now_add=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    # Test Parameters
+    heart_rate_avg = models.BooleanField(default=False)
+    heart_rate_max = models.BooleanField(default=False)
+    blood_pressure = models.BooleanField(default=False)
+    blood_glucose = models.BooleanField(default=False)
+    steps = models.BooleanField(default=False)
+    oxygen_data = models.BooleanField(default=False)
+    burned_calories = models.BooleanField(default=False)
+
 
 class ExperimentGroup(models.Model):
     name = models.CharField(max_length=100)
